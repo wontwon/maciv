abb8475ecee1d074f8f3bdd0e36b7fe2b1bc9da2
teevo <teevo714@gmail.com>
2014-08-06 02:18:57 -0400

    Add Angular to Admin Console; Add sidebar navigation; Add CSS/javascript references to Bootstrap and Angular;

--- engines/admin_console/app/controllers/admin_console/home_controller.rb
+++ engines/admin_console/app/controllers/admin_console/home_controller.rb
@@ -3,7 +3,7 @@ require_dependency "admin_console/application_controller"
 module AdminConsole
   class HomeController < ApplicationController
     def index
-      render 'home/index'
+      render 'admin_console/home/index'
     end
   end
 end
--- engines/admin_console/lib/admin_console.rb
+++ engines/admin_console/lib/admin_console.rb
@@ -1,3 +1,7 @@
+#Gem.loaded_specs['admin_console'].dependencies.each do |d|
+#  require d.name
+#end
+
 require "admin_console/engine"
 
 module AdminConsole
